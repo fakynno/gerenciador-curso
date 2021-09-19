04adf87c54b91c64066f3ffb2059db1637ccddd5
fakynno <fakynno@gmail.com>
2021-09-19 10:47:59 -0300

    Modulo 02 do curso

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
